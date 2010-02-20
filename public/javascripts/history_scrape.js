4fc39f2651e9cf865babe878e9c229d33a6430e1
Sai Emrys <github@saizai.com>
2010-02-20 02:53:08 -0800

    thanks; fixed MSIE compatibility at cost of 17% worse scraping performance; TODO: fix performance :-P

--- public/javascripts/history_scrape.js
+++ public/javascripts/history_scrape.js
@@ -22,24 +22,26 @@ CSSHistory.prep = function() {
 };
 
 CSSHistory.check = function(url) {
-	/* create the new anchor tag with the appropriate URL information */
-	var link = document.createElement("a");
-	// link.id = hex_md5(url); // since we are operating on any number of unordered links, this uses a hash to generate the ID
-	// link.innerHTML = url;
-	link.className = 'csshistory';
-	
+	var found = false;
+	var prefixes = ['http://', 'https://', 'http://www.', 'https://www.'];
 	var hex_color = '#ff0000'
 	var rgb_color = 'rgb(255, 0, 0)'
 	
-	/* quickly add and remove the link from the DOM with enough time to save the visible computed color. */
-	document.body.appendChild(link);
-	// document.defaultView is not supported by IE, nor is getComputedStyle. So for IE we use currentStyle instead.
-	// Cf. http://www.quirksmode.org/dom/w3c_html.html http://www.quirksmode.org/dom/w3c_css.html 
-	
-	var found = false;
-	var prefixes = ['http://', 'https://', 'http://www.', 'https://www.'];
-	
-	for (i = 0; i < prefixes.length; i++){
+	for (i = 0; !found && (i < prefixes.length); i++){
+		// create the new anchor tag with the appropriate URL information
+		// NOTE: MSIE has a bug whereby it does NOT update an element's style once created if the href changes.
+		// Therefore, we have to do a full setup/teardown each time.
+		// The efficiency reduction appears to be approx. 17%. :(
+		var link = document.createElement("a");
+		// link.id = hex_md5(url); // since we are operating on any number of unordered links, this uses a hash to generate the ID
+		// link.innerHTML = url;
+		link.className = 'csshistory';
+		
+		/* quickly add and remove the link from the DOM with enough time to save the visible computed color. */
+		document.body.appendChild(link);
+		// document.defaultView is not supported by IE, nor is getComputedStyle. So for IE we use currentStyle instead.
+		// Cf. http://www.quirksmode.org/dom/w3c_html.html http://www.quirksmode.org/dom/w3c_css.html 
+		
 		link.href = prefixes[i] + url;
 	    
 		// Version 2 - modified from AttackAPI
@@ -48,6 +50,7 @@ CSSHistory.check = function(url) {
 	    } else {
 			found = found || (document.defaultView.getComputedStyle(link, null).getPropertyValue('display') == 'none');
 		}
+		
 		/*
 		// Version 1 - modified from the original hack
 		if(document.defaultView)
@@ -56,8 +59,9 @@ CSSHistory.check = function(url) {
 			var color = link.currentStyle['color'];
 		found = found || color == rgb_color) || (color == hex_color);
 		*/
+		
+		document.body.removeChild(link);
 	}
-	document.body.removeChild(link);
 	
 	return found;
 };
