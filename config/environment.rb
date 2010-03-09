0bfafa3972e9ce8e9e75d8451d6f1e91f362a9ba
Sai Emrys <github@saizai.com>
2010-03-09 06:19:59 -0800

    production workling settings

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   config.gem 'dancroak-webster', :lib => 'webster', :source => 'http://gems.github.com'
   config.gem 'color-tools', :lib => 'color', :source => 'http://gems.github.com'
   config.gem 'tomz-libsvm-ruby-swig', :lib => 'svm', :source => 'http://gems.github.com'
+  config.gem 'scrubyt'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
