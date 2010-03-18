9db5791ff63a8496f42d43ec4b4b22a69314a460
Sai Emrys <github@saizai.com>
2010-03-18 04:17:38 -0700

    move graph colors to global table; make colors dynamically assigned

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,7 +7,7 @@ class MainController < ApplicationController
     # avg_visited > 0.1 is cheaper than doing "avg_visited > 0 and visited_users_count > #{(users_count / 10).round}"
     @popular_sites = Site.find(:all, :conditions => "avg_visited > 0.1", :order => 'avg_visited desc', :select => 'avg_visited, url')
     @used_agents = BrowserTest.used_agents
-    @used_methods = BrowserTest.used_agents
+    @used_methods = BrowserTest.used_methods
   end
   
 end
